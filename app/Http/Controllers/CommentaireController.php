d20e1e6d8cdfeedf032019523ac9ed74ef04cb68
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-08-05 01:28:59 +0100

    CRUD for Commentaire Model

--- app/Http/Controllers/CommentaireController.php
+++ app/Http/Controllers/CommentaireController.php
@@ -3,8 +3,76 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Commentaire;
 
 class CommentaireController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'article_id' => 'required|integer',
+            'content' => "string"
+        ]);
+
+        if($validator->fails())
+            return response()->json(['errors' => $validator->errors()] , 422);
+
+        $commentaire = Commentaire::create($request->all());
+
+        if($commentaire)
+            return response()->json(['message' => 'commentaire ajouté avec succes'],200) ;
+        
+        return response()->json(['message' => 'Erreur lors de l\'ajout'],200) ;
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'id' => 'required',
+            'article_id' => 'required|integer',
+            'content' => "string"
+        ]);
+
+        if($validator->fails())
+            return response()->json(['errors' => $validator->errors()] , 422);
+
+        $commentaire = Commentaire::find($request->input('id'));
+
+        if(!$commentaire)
+            return response()->json(['message' => "Commentaire n\'existe"],200);
+        
+        if($commentaire->update($request->all()))
+            return response()->json(['message' => "Mise à jour reussir"],200);
+
+        return response()->json(['message' => "Erreur lors de la mise à jour reussir"],200);
+    }
+
+    public function destroy($commentaire)
+    {
+        if(!$commentaire)
+            return response()->json(['message' => "Commentaire n\'existe"],200);
+
+        if($commentaire->delete())
+            return response()->json(['message' => "Commentaire supprimer avec succes"],200);
+
+        return response()->json(['message' => "Erreur lors de la suppression"],200);
+    }
+
+    public function getCommentaire($id)
+    {
+        $commentaire = Commentaire::find($id);
+
+        if(!$commentaire)
+            return response()->json(['message' => "Ce commentaire n'existe pas"],200);
+        
+        return response()->json($commentaire,200);
+    }
+
+    public function getCommentaires($article_id)
+    {
+        $commentaires = Commentaire::where('article_id',$article_id);
+
+        return response()->json($commentaires,200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentaireController;
 
 Route::delete('/article/{article}', [ArticleController::class , 'destroy']);
 Route::post('/article' , [ArticleController::class , 'create']) ;
 Route::get('/article/{id}' , [ArticleController::class , 'getArticle']);
 Route::get('/articles' , [ArticleController::class , 'getArticles']);
 Route::put('/article' , [ArticleController::class , 'update']);
+
+Route::delete('/commentaire/{commentaire}', [CommentaireController::class , 'destroy']);
+Route::post('/commentaire' , [CommentaireController::class , 'create']) ;
+Route::get('/commentaire/{id}' , [CommentaireController::class , 'getcommentaire']);
+Route::get('/commentaires' , [CommentaireController::class , 'getcommentaires']);
+Route::put('/commentaire' , [CommentaireController::class , 'update']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
